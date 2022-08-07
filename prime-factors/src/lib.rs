448cc24dff7665f924ee663ad2de077b0fbd1ac3
10EastSea <xgf6580@gmail.com>
2022-08-07 09:34:10 +0000

    prime-factors

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,3 +1,15 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {}", n)
+    let mut num = n;
+    let mut results = Vec::new();
+    
+    let mut i = 2;
+    while num > 1 {
+        while num % i == 0 {
+            results.push(i);
+            num /= i
+        }
+        i += 1
+    }
+
+    return results;
 }
